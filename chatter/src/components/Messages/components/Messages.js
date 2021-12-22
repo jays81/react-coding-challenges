08fada1a2289f54725a0b434c06deba259902bcc
jays81 <jays81@yahoo.com>
2021-12-22 17:38:47 +0000

    add functionality to meesages class and modify footer to allow message value to be set

--- chatter/src/components/Messages/components/Messages.js
+++ chatter/src/components/Messages/components/Messages.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState, useRef } from 'react';
 import io from 'socket.io-client';
 import useSound from 'use-sound';
 import config from '../../../config';
@@ -9,22 +9,119 @@ import Footer from './Footer';
 import Message from './Message';
 import '../styles/_messages.scss';
 
+const BOT = 'bot';
+const BOTTY_USER = 'botty';
+const ME = 'me';
+
 const socket = io(
   config.BOT_SERVER_ENDPOINT,
   { transports: ['websocket', 'polling', 'flashsocket'] }
 );
 
+
 function Messages() {
+
+
   const [playSend] = useSound(config.SEND_AUDIO_URL);
   const [playReceive] = useSound(config.RECEIVE_AUDIO_URL);
-  const { setLatestMessage } = useContext(LatestMessagesContext);
+  const { messages, setLatestMessage } = useContext(LatestMessagesContext);
+  const [message, setMessage] = useState("");
+  const stateRef = useRef();
+  const bottomRef = useRef();
+
+  const [messageList, setMessageList] = useState([{
+    id: BOT,
+    user: BOTTY_USER,
+    message: messages[BOT]
+  
+  }]);
+
+  stateRef.current = messageList;
+
+  const scrollToBottom = () => {
+    bottomRef.current.scrollIntoView({
+    behavior: "smooth",
+    block: "start",
+    });
+  };
+
+  useEffect(() => {
+    socket.on("bot-message", (data) => {
+      const objIndex =  stateRef.current.findIndex((obj => obj.isTyping === true));
+      const messageData = {
+        id: BOT,
+        user: BOTTY_USER,
+        message: data,
+        isTyping: false
+      };
+
+      stateRef.current.splice(objIndex, 1);
+      setLatestMessage(messageData.id, messageData.message);
+      setMessageList((list) => [...list, messageData]);
+      scrollToBottom();
+      playReceive();
+    });
+
+    socket.on("bot-typing", (data) => {
+      const messageData = {
+        id: BOT,
+        user: BOTTY_USER,
+        message: "",
+        isTyping: true
+      };
+
+      setMessageList((list) => [...list, messageData]);
+
+      scrollToBottom();
+       
+     });
+
+     
+
+  }, [socket]);
+
+  const onChangeMessage = (e) => {
+     setMessage(e.target.value);    
+  };
+  
+
+  const sendMessage = async () => {
+    const messageData = {
+      id: ME,
+      message: message,
+      user: ME
+    };
+
+
+    await socket.emit('user-message', message); 
+    setLatestMessage(messageData.id, messageData.message);
+    setMessageList((list) => [...list, messageData]);
+    setMessage(""); 
+    scrollToBottom();
+    playSend();
+    
+    
+  }
 
   return (
     <div className="messages">
       <Header />
       <div className="messages__list" id="message-list">
+      {
+        messageList.map(message => {
+          if(message.isTyping){
+            return <TypingMessage />
+          }
+          if(message.message){
+            return <Message  message={message} />
+          }
+        })
+      }
+
+      <div ref={bottomRef} className="list-bottom"></div>
+
       </div>
-      <Footer message={message} sendMessage={sendMessage} onChangeMessage={onChangeMessage} />
+       <Footer message={message} sendMessage={sendMessage} onChangeMessage={onChangeMessage} /> 
     </div>
   );
 }
